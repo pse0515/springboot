adb7a5dfbf870990c8a8786e8a6800813165ad06
pse0515 <pse12345665@gmail.com>
2025-12-02 16:29:56 +0900

    채ㅜã컨트롤러 수업

---
+++ src/main/java/com/korit/springboot/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.korit.springboot.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+// 1
+@RestController
+public class UserController {
+
+    private String username = "test12";
+    private String password = "1234";
+
+    @GetMapping("/info")
+    public ResponseEntity<String> printInfo() {
+        return ResponseEntity.ok("UserController!!!");
+    }
+
+
+//    @GetMapping("/users")
+//    public Map<String,String> getUsers(HttpServletResponse response) {
+//        response.setStatus(400);
+//        response.setContentType("application/json");
+//        return Map.of("username",username, "password", password);
+//    }
+
+    @GetMapping("/users")
+    public ResponseEntity<Map<String,String>> getUsers(HttpServletResponse response) {
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("username",username, "password", password));
+    }
+
+}
---
+++ src/main/java/com/korit/springboot/dto/package-info.java
@@ -0,0 +1,4 @@
+package com.korit.springboot.dto;
+/**
+ * 패키지 생성 순서 : 2
+ */
